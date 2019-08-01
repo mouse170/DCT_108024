03294dce2226d6e9492fb1f94a5989b85c605cd0
Chu Hung Lin <mouse170@gmail.com>
2019-08-01 13:26:05 +0800

    routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,9 +10,18 @@ const routes: Routes = [
     path: '',
     component: LayoutComponent,
     children: [
-      { path: '', component: DashboardComponent },
-      { path: 'page1', component: Page1Component },
-      { path: 'page2', component: Page2Component }
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'page1',
+        component: Page1Component
+      },
+      {
+        path: 'page2',
+        component: Page2Component
+      }
     ]
   }
 ];
